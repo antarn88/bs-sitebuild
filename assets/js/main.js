e470144da70ab822d8ebc3d40b58e409073dd76e
Arnold <antaloczi.arnold@gmail.com>
2021-01-10 19:37:55 +0100

    Added scroll animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,24 @@
+'use strict';
+
+// Tooltip
 $(function () { $('[data-toggle="tooltip"]').tooltip() });
+
+// Modals
 document.querySelector('.privacy').addEventListener('click', () => $('#privacyModal').modal('show'));
 document.querySelector('.terms').addEventListener('click', () => $('#termsModal').modal('show'));
 document.querySelector('.faq').addEventListener('click', () => $('#faqModal').modal('show'));
+
+// Scrool animation
+$(document).ready(function () {
+    $('a').on('click', function (event) {
+        if (this.hash !== '') {
+            event.preventDefault();
+            const hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 300, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
